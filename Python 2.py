4893459eba3dffb8218a9f0486ee07635a2326ee
AdilAbayevich <92146755+AdilAbayevich@users.noreply.github.com>
2021-10-08 13:00:49 +0600

    Add files via upload

---
+++ Python 2.py
@@ -0,0 +1,14 @@
+Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python38/imkuyiku.py ==
+Введите первое число:10
+Введите второе число:20
+Result: 70
+Result: 30
+Result: 2.5
+Result: 1000
+Result: 9536743164062500000000000000000000
+Result: 2
+HiHi
+>>> 
